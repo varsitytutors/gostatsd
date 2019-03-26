3a9f97b014cc4723279fb9c95948a5c1d4c2b55f
Corey Mangum <corey.mangum@varsitytutors.com>
2019-03-25 19:08:09 -0700

    copy config file

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 mkdir -p /opt/gostatsd
 systemctl stop gostatsd
 cp gostatsd /opt/gostatsd
+cp gostatsd.toml /opt/gostatsd
 cp gostatsd.service /etc/systemd/system/gostatsd.service
 systemctl enable gostatsd
 systemctl start gostatsd
