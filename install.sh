a4803e9edbfa31c243cec04356459cfebae2f9cd
Corey Mangum <corey.mangum@varsitytutors.com>
2019-03-25 18:16:27 -0700

    add service install and config

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p /opt/gostatsd
+systemctl stop gostatsd
+cp gostatsd /opt/gostatsd
+cp gostatsd.service /etc/systemd/system/gostatsd.service
+systemctl enable gostatsd
+systemctl start gostatsd
